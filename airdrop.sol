f3e0f6b02ebdfa04f989ded224c8e6b2ec3e5e70
Anjali Sharma <92917156+anjali-wdcs@users.noreply.github.com>
2022-03-16 18:23:14 +0545

    airdrop

---
+++ airdrop.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract Token {
+    function transfer(address to, uint amount) public virtual returns(bool);
+    event Transferred(address indexed from, address indexed to, uint indexed amount);
+}
+
+contract creation is Ownable {
+    Token public tokInstance;
+    
+    constructor(address _tokAdr) {
+        tokInstance = Token(_tokAdr);
+    }
+    
+   
+   function airdropTokens(address[] memory recipients, uint[] memory amount) public onlyOwner {
+        for(uint i=0; i < recipients.length; i++) {
+            require(recipients[i] != address(0));
+            tokInstance.transfer(recipients[i], amount[i]);
+        }
+    }    
+    
+    function sendBatch(address[] memory recievers, uint[] memory amount) public onlyOwner returns(bool) {
+        require(recievers.length == amount.length);
+        for(uint i=0; i < amount.length; i++){
+            tokInstance.transfer(recievers[i], amount[i]);
+        }
+        
+        return true;
+    }
+}
+
